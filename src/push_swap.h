2b7858b33059e6a113347ade9e1c4ce299bc3db2
Takumasa Matsuura <tamatsuu@student.42tokyo.jp>
2024-08-18 21:04:22 +0900

    add submodule repo in libft

--- src/push_swap.h
+++ src/push_swap.h
@@ -1,34 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tamatsuu <tamatsuu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/17 02:44:28 by tamatsuu          #+#    #+#             */
-/*   Updated: 2024/08/18 20:59:45 by tamatsuu         ###   ########.fr       */
+/*   Created: 2024/08/18 20:51:48 by tamatsuu          #+#    #+#             */
+/*   Updated: 2024/08/18 20:52:43 by tamatsuu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
-int	main(int argc, char *argv[])
-{
-	void *init_data;// this data should be 
 
-	validate_args(argc, argv);
-	push_swap(argv);
-	return (0);
-}
 
-push_swap(char **argv)
-{
-	int		arg_size;
-	int		arry[500];
-	t_list	stack_a;
-	t_list	stack_b;
-	
-	arg_size = 0;
- 	transform_arg_to_arry(argv, &arry, sizeof(arry)/sizeof(int), &arg_size);
-	create_index(&arry, arg_size);
-	sort_operation();
-}
+#endif
