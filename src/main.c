621f6defac2c2b2fe02bab7b7938ef5199bdf099
Takumasa Matsuura <tamatsuu@student.42tokyo.jp>
2024-08-18 23:48:59 +0900

    write some psedu code

--- src/main.c
+++ src/main.c
@@ -6,29 +6,56 @@
 /*   By: tamatsuu <tamatsuu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 02:44:28 by tamatsuu          #+#    #+#             */
-/*   Updated: 2024/08/18 20:59:45 by tamatsuu         ###   ########.fr       */
+/*   Updated: 2024/08/18 23:47:31 by tamatsuu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../libft/libft.h"
+#include "push_swap.h"
 
 int	main(int argc, char *argv[])
 {
-	void *init_data;// this data should be 
-
 	validate_args(argc, argv);
-	push_swap(argv);
+	init(argv);
 	return (0);
 }
 
-push_swap(char **argv)
+int	init(char **argv)
 {
 	int		arg_size;
-	int		arry[500];
-	t_list	stack_a;
-	t_list	stack_b;
-	
+	int		arry[ARRY_SIZE];
+	t_list	*stack_a;
+	t_list	*stack_b;
+	t_list	*op_list;
+
 	arg_size = 0;
- 	transform_arg_to_arry(argv, &arry, sizeof(arry)/sizeof(int), &arg_size);
-	create_index(&arry, arg_size);
-	sort_operation();
+	stack_a = ft_lstnew(NULL);
+	stack_b = ft_lstnew(NULL);
+	op_list = ft_lstnew(NULL);
+	if (!stack_a || !stack_b)
+		throw_err(MEM_ALLOCATION_ERR);
+	tsfrm_arg_to_arry(argv, &arry, ARRY_SIZE, &arg_size);
+	init_stack(stack_a, arry, arg_size); //push array value into stack_a
+	create_index(&arry, arg_size); //sort arry and create index
+	push_swap(stack_a, stack_b, op_list, arry);
+}
+
+int	push_swap(t_list *stack_a, t_list *stack_b, t_list *op_lst, int *arry)
+{
+	int	lst_size;
+
+	lst_size = ft_lstsize(stack_a);
+}
+
+void	split_chunk(t_list *stack_a, t_list *stack_b, int mid_val, t_split_info *meta)
+{
+	if (base_case())
+	{
+		basic_sort(stack_a, stack_b);
+		return ;
+	}
+	split_operation(stack_a,stack_b, meta);
+	split_chunk(stack_a,stack_b, meta);
+	split_chunk(stack_a,stack_b, meta);
+	split_chunk(stack_a,stack_b, meta);
 }
--- src/push_swap.h
+++ src/push_swap.h
@@ -6,13 +6,15 @@
 /*   By: tamatsuu <tamatsuu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 20:51:48 by tamatsuu          #+#    #+#             */
-/*   Updated: 2024/08/18 20:52:43 by tamatsuu         ###   ########.fr       */
+/*   Updated: 2024/08/18 21:09:07 by tamatsuu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# define ARRY_SIZE 500
 
+# define MEM_ALLOCATION_ERR -1
 
 #endif
